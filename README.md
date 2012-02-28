2c4a45113216273099ed3292db9ae4b44ccffe38
François Benaiteau <francois.benaiteau@gmail.com>
2012-02-28 23:27:36 +0100

    Implement the way multiple .strings to csv. Moved to module

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ This script converts a csv file of translations into iOS .strings files and vice
 *Needs fastercsv
 
 =Usage=
+Convert from CSV file to .strings files 
  ruby convert.rb <filename>.csv
 
+Convert from .strings files to CSV file 'Translations.csv'
+ ruby convert.rb <filename1>.strings <filename2>.strings
 The csv file needs one column per language and must have headers (for output filenames)
 
 =Todo=
-*Implement the way multiple .strings to csv.
+*Add option to choose the reference column to take in account
+*Add option for output CSV file
+*Add tests suite
--- convert.rb
+++ convert.rb
@@ -2,86 +2,95 @@
 
 require 'rubygems'
 begin
-  require 'faster_csv'
+	require 'faster_csv'
 rescue LoadError
-  puts "FasterCSV required! i.e: gem install fastercsv"
-  exit
-end
-#variables
-file                                     = nil
-basename                                 = nil
-
-#Functions
-def dotstrings_to_csv(filenames)
-  puts "not yet implemented"
-  exit
-  
-  
-  newfile                                = 'Translations.csv'
-  common_strings                         = []
-  
-  filenames.each do |fname|
-
-    FasterCSV.foreach(fname, :quote_char => '"', :col_sep =>'=', :row_sep =>';') do |row|
-
-    end
-
-
-  end
-end  
-
-def csv_to_dotstrings(name)
-  files = {}
-  rowIndex = 0
-  FasterCSV.foreach(name, :quote_char    => '"', :col_sep =>',', :row_sep =>:auto) do |row|
-    if rowIndex == 0
-      return unless row.count > 1 #check there's at least two columns
-    else
-      next if row == nil #skip empty lines (or sections)
-    end
-    row.size.times do |i|
-      if rowIndex == 0 #header
-        filename = "#{row[i]}.strings"
-        puts ">>>Creating file : #{filename}"
-        files[i] = File.new(filename,"w")
-      else
-        value = row[0].strip #@todo: add option to strip the constant or referenced language
-        files[i].write "\"#{value}\" = \"#{row[i]}\";\n"
-      end
-    end
-    rowIndex+=1
-  end
-  puts ">>>Created #{files.size} files. Content: #{rowIndex - 1} translations"
-  files.each do |fileIndex,fpointer|
-    fpointer.close
-  end
-
+	puts "FasterCSV required! i.e: gem install fastercsv"
+	exit
 end
 
-def usage
-  puts "Usage:\n" #@todo: add details about arguments
+module CSVStringsConverter
+	# Convert Localizable.strings files to one CSV file
+	def self.dotstrings_to_csv(filenames)
+		newfile                                = 'Translations.csv'
+		common_strings                         = {}
+		headers                                = ["\"Common Constants\""]
+    
+		# Parse .strings files
+		filenames.each do |fname|
+			header = fname.split('.')[0] if fname
+			headers << "\"#{header}\""
+			puts "Parsing filename : #{fname}"
+			FasterCSV.foreach(fname, :quote_char => '"', :col_sep =>' = ', :row_sep =>";\n") do |row|
+				common_strings[row[0].to_sym] = [] unless common_strings[row[0].to_sym]
+				if row.size == 2
+					common_strings[row[0].to_sym] <<  row[1]
+				else
+					puts "dot file no well formatted"
+				end
+			end
+		end
+
+		# Create csv file
+		puts "Create csv file"
+		output = File.new(newfile, "w")
+		output.write headers.join(',') + "\n"
+		common_strings.each do |key, row|
+			output.write "\"#{key}\",\"#{row.join('","')}\"\n"
+		end
+		output.close
+	end  
+
+	# Convert csv file to multiple Localizable.strings files for each column
+	def self.csv_to_dotstrings(name)
+		files = {}
+		rowIndex = 0
+		FasterCSV.foreach(name, :quote_char    => '"', :col_sep =>',', :row_sep =>:auto) do |row|
+			if rowIndex == 0
+				return unless row.count > 1 #check there's at least two columns
+			else
+				next if row == nil #skip empty lines (or sections)
+			end
+			row.size.times do |i|
+				if rowIndex == 0 #header
+					filename = "#{row[i]}.strings"
+					puts ">>>Creating file : #{filename}"
+					files[i] = File.new(filename,"w")
+				else
+					value = row[0].strip #@todo: add option to strip the constant or referenced language
+					files[i].write "\"#{value}\" = \"#{row[i]}\";\n"
+				end
+			end
+			rowIndex += 1
+		end
+		puts ">>>Created #{files.size} files. Content: #{rowIndex - 1} translations"
+		files.each do |fileIndex,fpointer|
+		  fpointer.close
+		end
+
+	end
 end
 
-def checks
-  if ARGV.size < 1
-    puts "Error: not enough arguments"
-    usage
-    exit
-  end
-  ARGV.each do |filename|
-    if File.extname(filename).downcase == '.csv'
-      csv_to_dotstrings(filename)
-      return
-    end
+# Part of the script
+if $0 == __FILE__  
+	# Shows help on how to use this script
+	def usage
+		puts "Usage:\n" #@todo: add details about arguments
+		puts "ruby convert.rb <filename.csv>"
+		puts "ruby convert.rb <filename1.strings> <filename2.strings>"
+		exit
+	end
 
-    if File.extname(filename).downcase != '.strings'
-      puts "strings"
-      return usage
-    end
-    dotstrings_to_csv(ARGV)
-  end 
+	# Main program
+	if ARGV.size < 1
+		puts "Error: not enough arguments"
+		usage
+	elsif filename = ARGV[0] and ARGV.size == 1 and File.extname(filename).downcase == '.csv'
+		CSVStringsConverter.csv_to_dotstrings(filename)
+	else
+		# checks arguments
+		ARGV.each do |filename|
+			usage if File.extname(filename).downcase != '.strings'
+		end 
+		CSVStringsConverter.dotstrings_to_csv(ARGV)
+	end
 end
-#main
-checks
-
-
