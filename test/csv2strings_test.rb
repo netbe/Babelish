5d9063dd48580c068f0329a73efdce0c28a8d6a1
François Benaiteau <francois.benaiteau@gmail.com>
2012-10-04 17:05:39 +0200

    Cleanup files for testing

--- test/csv2strings_test.rb
+++ test/csv2strings_test.rb
@@ -1,15 +1,4 @@
-begin
-  require 'simplecov'
-  SimpleCov.start do 
-     add_filter "test"
-  end
-rescue LoadError
-  puts 'Coverage disabled, enable by installing simplecov'
-end
-
-
-require 'test/unit'
-require "mocha"
+require 'test_helper'
 require 'csv2strings'
 
 
