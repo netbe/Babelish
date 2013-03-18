55cfed3b5ad9f28eea366741d8756cc909552cf2
François Benaiteau <francois.benaiteau@gmail.com>
2013-03-19 00:44:15 +0100

    csv2strings: langs parameter required

--- lib/csv2strings/converter.rb
+++ lib/csv2strings/converter.rb
@@ -1,63 +1,43 @@
 module CSV2Strings
 	class Converter
 		attr_accessor :csv_filename, :output_file
+		attr_accessor :langs, :default_lang
+		attr_accessor :default_path
+		attr_accessor :excluded_states, :state_column, :keys_column
 
-		def initialize(filename, args = {})
+
+		def initialize(filename, langs, args = {})
+			args.merge!({
+			 :excluded_states => [],
+			 :state_column => nil,
+			 :keys_column => 0})
+
 			@csv_filename = filename
-			if self.class.langs.size == 0
-				@output_file = args[:output_file]
-			else
-				@output_file = nil
+			@langs = langs
+
+			if !@langs.is_a?(Hash) || @langs.size == 0
+				raise "wrong format or/and languages parameter" 
 			end
+			@output_file = (@langs.size == 1) ? args[:output_file] : nil
+
+			@default_path = args[:default_path].to_s
+			@excluded_states = args[:excluded_states]
+			@state_column = args[:state_column]
+			@keys_column = args[:keys_column]
+			@default_lang = args[:default_lang]
 		end
 
-		def csv_to_dotstrings
-			self.class.csv_to_dotstrings(self.csv_filename)
-		end
-		
-		def self.excluded_states
-			return CSV2StringsConfig[:excluded_states] if defined?(CSV2StringsConfig)
-			[]
-		end
-		
-		def self.state_column
-			return CSV2StringsConfig[:state_column] if defined?(CSV2StringsConfig)
-			10  
-		end
-
-		def self.keys_column
-			return CSV2StringsConfig[:keys_column] if defined?(CSV2StringsConfig)
-			0
-		end
-
-		def self.default_lang
-			return CSV2StringsConfig[:default_lang] if defined?(CSV2StringsConfig)
-			'English' 
-		end
-
-		def self.langs
-			return CSV2StringsConfig[:langs] if defined?(CSV2StringsConfig)
-			{'English'  => [:en]}
-		end
-
-		def self.default_path
-			return CSV2StringsConfig[:path] if defined?(CSV2StringsConfig)
-			""
-		end
-
-		def self.create_file_from_path(file_path)
+		def create_file_from_path(file_path)
 			path = File.dirname(file_path)
-			# filename = File.basename(file_path)
 			FileUtils.mkdir_p path
 			return File.new(file_path,"w")
 		end
 
-		def self.process_header(excludedCols, defaultCol, files, row, index)
+		def process_header(excludedCols, defaultCol, files, row, index)
 			defaultCol = index if self.default_lang == row[index]
-			files[index]   = []
-			puts self.langs
+			files[index] = []
 			lang_index = row[index]
-
+			
 			# create output files here
 			if @output_file
 				# one single file
@@ -65,58 +45,61 @@ module CSV2Strings
 			else
 				# create one file for each languages
 				self.langs[lang_index].each do |locale|
-					# get file_path for locale
 					filename = self.file_path_for_locale(locale)
 					files[index] << self.create_file_from_path(filename)
 				end
 			end
 		end
 
-		def self.file_path_for_locale(locale)
+		def file_path_for_locale(locale)
 			require 'pathname' 
 			Pathname.new(self.default_path) + "#{locale}.lproj" + "Localizable.strings"
 		end
 		
-		def self.process_value(row_value, default_value)
+		def process_value(row_value, default_value)
 			value = row_value.nil? ? default_value : row_value
 			value = "" if value.nil?
 			value.gsub!(/\\*\"/, "\\\"") #escape double quotes
 			value.gsub!(/\s*(\n|\\\s*n)\s*/, "\\n") #replace new lines with \n + strip
 			value.gsub!(/%\s+([a-zA-Z@])([^a-zA-Z@]|$)/, "%\\1\\2") #repair string formats ("% d points" etc)
 			value.gsub!(/([^0-9\s\(\{\[^])%/, "\\1 %")
-			value.strip!
-			return value
-		end
+				value.strip!
+				return value
+			end
 
 		# Convert csv file to multiple Localizable.strings files for each column
-		def self.csv_to_dotstrings(name)
+		def csv_to_dotstrings(name = self.csv_filename)
 			files        = {}
 			rowIndex     = 0
 			excludedCols = []
 			defaultCol   = 0
+			nb_translations = 0
 			CSVParserClass.foreach(name, :quote_char => '"', :col_sep =>',', :row_sep => :auto) do |row|
 				if rowIndex == 0
 					return unless row.count > 1 #check there's at least two columns
 				else
 					next if row == nil or row[self.keys_column].nil? #skip empty lines (or sections)
 				end
+
 				row.size.times do |i|
 					next if excludedCols.include? i
 					if rowIndex == 0 #header
-						excludedCols << i and next unless self.langs.has_key?(row[i])
+						# ignore all headers not listed in langs to create files
+						(excludedCols << i and next) unless self.langs.has_key?(row[i])
 						self.process_header(excludedCols, defaultCol, files, row, i)
-					elsif row[self.state_column].nil? or row[self.state_column] == '' or !self.excluded_states.include? row[self.state_column]
+					elsif !self.state_column || (row[self.state_column].nil? or row[self.state_column] == '' or !self.excluded_states.include? row[self.state_column])
 						# TODO: add option to strip the constant or referenced language
 						key = row[self.keys_column].strip 
 						value = self.process_value(row[i], row[defaultCol])
 						files[i].each do |file|
+							nb_translations += 1
 							file.write "\"#{key}\" = \"#{value}\";\n"
 						end
 					end
 					rowIndex += 1
 				end
 			end
-			puts ">>>Created #{files.size} files. Content: #{rowIndex - 1} translations"
+			puts "\n>>>Created #{files.size} files. Content: #{nb_translations} translations\n"
 
 			# closing I/O
 			files.each do |key,locale_files|
@@ -129,4 +112,3 @@ module CSV2Strings
 
 	end # end of class
 end
-	
