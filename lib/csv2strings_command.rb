277a31a14a31f4ee19e121a349423f0f073d294b
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2013-10-08 18:23:57 +0200

    Extract puts in classes to command

--- lib/csv2strings_command.rb
+++ lib/csv2strings_command.rb
@@ -35,7 +35,7 @@ class CSV2StringsCommand < Command
     args.delete(:langs)
     args.delete(:filename)
     converter = CSV2Strings::Converter.new(filename, options[:langs], args)
-    converter.csv_to_dotstrings    
+    say converter.csv_to_dotstrings    
   end
 
 end
