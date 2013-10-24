02e2a04753884d27917ba831fe8bc4082c23e3af
François Benaiteau <francois.benaiteau@gmail.com>
2013-10-25 01:35:46 +0200

    Revert "Move coveralls to top of test_helper"
    This reverts commit 4851fb6c02cdd40d062a5a7ece06e5f0b4a782ef.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+begin
+  require 'simplecov'
+  SimpleCov.start do
+     add_filter "test"
+  end
+rescue LoadError
+  puts 'Coverage disabled, enable by installing simplecov'
+end
+
 require 'coveralls'
 Coveralls.wear!
 
