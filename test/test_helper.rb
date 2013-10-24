4851fb6c02cdd40d062a5a7ece06e5f0b4a782ef
François Benaiteau <francois.benaiteau@gmail.com>
2013-10-25 01:22:53 +0200

    Move coveralls to top of test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,3 @@
-begin
-  require 'simplecov'
-  SimpleCov.start do
-     add_filter "test"
-  end
-rescue LoadError
-  puts 'Coverage disabled, enable by installing simplecov'
-end
-
 require 'coveralls'
 Coveralls.wear!
 
