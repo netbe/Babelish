df55bc5088d38fb8eb6cc3dfae93b328a24a884a
François Benaiteau <francois.benaiteau@gmail.com>
2013-10-29 23:31:35 +0100

    ADD coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,14 @@
 begin
   require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
   SimpleCov.start do
      add_filter '/test/'
   end
 rescue LoadError
   puts 'Coverage disabled, enable by installing simplecov'
 end
-
 
 require 'test/unit'
 
