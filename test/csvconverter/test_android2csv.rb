7c6f9d6488aaceb8fe83d0cafe7717181d6487ba
netbe <francois.benaiteau@gmail.com>
2014-03-13 00:01:49 +0100

    remove tests covering base2csv

--- test/csvconverter/test_android2csv.rb
+++ test/csvconverter/test_android2csv.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 class TestAndroid2CSV < Test::Unit::TestCase
-  
+
   def test_load_strings_with_wrong_file
     assert_raise(Errno::ENOENT) do
       output = Android2CSV.new.load_strings "file that does not exist.strings"
@@ -11,31 +11,6 @@ class TestAndroid2CSV < Test::Unit::TestCase
     expected_output = {"app_name" => "android2csv", "action_greetings" => "Hello", "ANOTHER_STRING" => "testEN", "empty" => ""}
     output = Android2CSV.new.load_strings "test/data/android.xml"
     assert_equal expected_output, output
-  end
-
-  def test_xml_to_csv
-    omit
-    converter = Android2CSV.new(:filenames => ["test/data/android.xml"])
-    keys, lang_order, strings = converter.convert(false)
-    assert_equal ["android".to_sym], lang_order
-    assert_equal ["app_name", "action_greetings", "ANOTHER_STRING", "empty"], keys
-    expected_strings = {lang_order[0] => {"app_name" => "android2csv", "action_greetings" => "Hello", "ANOTHER_STRING" => "testEN", "empty" => ""}}
-    assert_equal expected_strings, strings
-  end
-
-  def test_create_csv_file
-    omit
-    keys = ["app_name", "action_greetings"]
-    lang_order = [:"android"]
-    strings = {lang_order[0] => {"app_name" => "android2csv", "action_greetings" => "Hello"}}
-    
-    converter = Android2CSV.new(:headers => %w{variables english})
-
-    converter.create_csv_file(keys, lang_order, strings)
-    assert File.exist?(converter.csv_filename)
-
-    #clean up
-    system("rm -rf ./" + converter.csv_filename)
   end
 
   def test_initialize
