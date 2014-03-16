e2554dcda788fa2f180ba27c081918db97f77fdc
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-03-16 21:37:07 +0100

    ruby 1.8.7 utf8 method

--- lib/csvconverter.rb
+++ lib/csvconverter.rb
@@ -17,6 +17,24 @@ end
 
 CSVParserClass = CSVGEM == 'csv' ? CSV : FasterCSV
 
+
+# Fixes UTF8 issues
+# see http://stackoverflow.com/questions/4583924/string-force-encoding-in-ruby-1-8-7-or-rails-2-x
+if RUBY_VERSION.match(/^[0-1]\.[0-8]\./)
+	require 'iconv'
+	class String
+	  def to_utf8
+	    ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', self + ' ')[0..-2]
+	  end
+	end
+else
+	class String
+	  def to_utf8
+	  	force_encoding("UTF-8")
+	  end
+	end
+end
+
 # From CSV
 require "csvconverter/csv2base"
 require "csvconverter/csv2strings"
@@ -34,4 +52,3 @@ require "csvconverter/json2csv"
 # General
 require "csvconverter/language"
 require "csvconverter/google_doc"
-
