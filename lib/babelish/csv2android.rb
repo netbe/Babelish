cb399afa44ff2cb1505db36148ad39b467778a59
Markus Paeschke <markus.paeschke@gmail.com>
2015-05-22 22:52:43 +0200

    Fixed a problem which occures if the original android xml file cointains text which is wrapped in quotes like in the test result xml file the node with the name good_example_3. Additionally created unit test to verify the functionality.

--- lib/babelish/csv2android.rb
+++ lib/babelish/csv2android.rb
@@ -16,6 +16,16 @@ module Babelish
       return filepath ? [filepath] : []
     end
 
+    def process_value(row_value, default_value)
+      value = super(row_value, default_value)
+      # if the value beginns and ends with a quote we must leave them unescapted
+      if value.size > 4 && value[0,2] == "\\\"" && value[value.size - 2, value.size] == "\\\""
+        value[0,2] = "\""
+        value[value.size - 2, value.size] = "\""
+      end
+      return value.to_utf8
+    end
+
     def get_row_format(row_key, row_value, comment = nil, indentation = 0)
         return "\t<string name=\"#{row_key}\">#{row_value}</string>\n"
     end
--- test/babelish/test_csv2android.rb
+++ test/babelish/test_csv2android.rb
@@ -11,6 +11,17 @@ class TestCSV2Android < Test::Unit::TestCase
     system("rm -rf ./values-en")
   end
 
+  def test_converting_csv_with_special_chars
+    csv_file = "test/data/android_special_chars.csv"
+    converter = Babelish::CSV2Android.new(csv_file, 'german' => "de")
+    converter.convert
+    assert File.exists?("values-de/strings.xml"), "the ouptut file does not exist"
+    assert FileUtils.compare_file("values-de/strings.xml", "test/data/android_special_chars_test_result.xml")
+
+    #clean up
+    system("rm -rf ./values-de")
+  end
+
   def test_converting_csv_to_dotstrings_one_output_option
     csv_file = "test/data/test_data.csv"
     single_file = 'myApp.xml'
