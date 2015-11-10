d5cc9e3b7e8d0d78b1e7971dce470c5a06d36548
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2015-11-10 11:34:46 +0100

    Use double quote as houndci states

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,24 +1,24 @@
 begin
-  require 'simplecov'
-  require 'coveralls'
+  require "simplecov"
+  require "coveralls"
 
   SimpleCov.formatter = Coveralls::SimpleCov::Formatter
   SimpleCov.start do
-     add_filter '/test/'
+     add_filter "/test/"
   end
 rescue LoadError
-  puts 'Coverage disabled, enable by installing simplecov'
+  puts "Coverage disabled, enable by installing simplecov"
 end
 
-require 'test/unit'
+require "test/unit"
 
-require 'babelish'
+require "babelish"
 
 require "babelish/commandline"
 
-require 'webmock/test_unit'
-require 'vcr'
+require "webmock/test_unit"
+require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = "test/fixtures/vcr_cassettes"
-  config.hook_into :webmock # or :fakeweb
+  config.hook_into :webmock
 end
