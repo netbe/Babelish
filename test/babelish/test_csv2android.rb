f90246cfddc6bcc7d044f79ca53e71af4746708a
Borut Tomažin <borut.tomazin@icloud.com>
2016-01-29 14:14:08 +0100

    Fixed style issue.

--- test/babelish/test_csv2android.rb
+++ test/babelish/test_csv2android.rb
@@ -4,7 +4,7 @@ class TestCSV2Android < Test::Unit::TestCase
   def test_converting_csv_to_xml
     csv_file = "test/data/test_data.csv"
     converter = Babelish::CSV2Android.new(csv_file,
-    "English" => "en")
+										  "English" => "en")
     converter.convert
     assert File.exist?("values-en/strings.xml"), "the ouptut file does not exist"
 
@@ -15,7 +15,7 @@ class TestCSV2Android < Test::Unit::TestCase
   def test_converting_csv_with_special_chars
     csv_file = "test/data/android_special_chars.csv"
     converter = Babelish::CSV2Android.new(csv_file,
-    "german" => "de")
+										  "german" => "de")
     converter.convert
     assert File.exist?("values-de/strings.xml"), "the ouptut file does not exist"
     assert_equal File.read("test/data/android_special_chars_test_result.xml"), File.read("values-de/strings.xml")
@@ -28,7 +28,7 @@ class TestCSV2Android < Test::Unit::TestCase
     csv_file = "test/data/test_data.csv"
     single_file = "myApp.xml"
     converter = Babelish::CSV2Android.new(csv_file,
-    { "English" => "en" },
+										  { "English" => "en" },
     :output_basename => "myApp",
     :ignore_lang_path => true)
     converter.convert
@@ -43,7 +43,7 @@ class TestCSV2Android < Test::Unit::TestCase
     french_file = "values-fr/strings.xml"
     expected_output = File.read("test/data/test_data_fr_with_comments.xml")
     converter = Babelish::CSV2Android.new(csv_file,
-    { "French" => "fr" },
+										  { "French" => "fr" },
     :default_lang => "English",
     :comments_column => 5)
     converter.convert
@@ -51,7 +51,7 @@ class TestCSV2Android < Test::Unit::TestCase
     result = File.read(french_file)
     assert_equal expected_output, result
 	
-	# clean up
+    # clean up
     system("rm -rf values-fr")
   end
 end
