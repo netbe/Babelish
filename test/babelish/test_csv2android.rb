4d3b1b1898f3c47520d61c4dbb4fab04dbca11ee
Borut Tomažin <borut.tomazin@icloud.com>
2016-01-29 14:19:26 +0100

    Fixed style issue.

--- test/babelish/test_csv2android.rb
+++ test/babelish/test_csv2android.rb
@@ -3,10 +3,10 @@ class TestCSV2Android < Test::Unit::TestCase
 
   def test_converting_csv_to_xml
     csv_file = "test/data/test_data.csv"
-    converter = Babelish::CSV2Android.new(csv_file,
-										  "English" => "en")
+    converter = Babelish::CSV2Android.new(csv_file, "English" => "en")
     converter.convert
-    assert File.exist?("values-en/strings.xml"), "the ouptut file does not exist"
+    assert File.exist?("values-en/strings.xml"),
+    "the ouptut file does not exist"
 
     # clean up
     system("rm -rf ./values-en")
@@ -14,11 +14,12 @@ class TestCSV2Android < Test::Unit::TestCase
 
   def test_converting_csv_with_special_chars
     csv_file = "test/data/android_special_chars.csv"
-    converter = Babelish::CSV2Android.new(csv_file,
-                                          "german" => "de")
+    converter = Babelish::CSV2Android.new(csv_file, "german" => "de")
     converter.convert
-    assert File.exist?("values-de/strings.xml"), "the ouptut file does not exist"
-    assert_equal File.read("test/data/android_special_chars_test_result.xml"), File.read("values-de/strings.xml")
+    assert File.exist?("values-de/strings.xml"),
+    "the ouptut file does not exist"
+    assert_equal File.read("test/data/android_special_chars_test_result.xml"),
+    File.read("values-de/strings.xml")
 
     # clean up
     system("rm -rf ./values-de")
@@ -27,8 +28,7 @@ class TestCSV2Android < Test::Unit::TestCase
   def test_converting_csv_to_dotstrings_one_output_option
     csv_file = "test/data/test_data.csv"
     single_file = "myApp.xml"
-    converter = Babelish::CSV2Android.new(csv_file,
-                                          { "English" => "en" },
+    converter = Babelish::CSV2Android.new(csv_file, { "English" => "en" },
     :output_basename => "myApp",
     :ignore_lang_path => true)
     converter.convert
@@ -42,8 +42,7 @@ class TestCSV2Android < Test::Unit::TestCase
     csv_file = "test/data/test_data_with_comments.csv"
     french_file = "values-fr/strings.xml"
     expected_output = File.read("test/data/test_data_fr_with_comments.xml")
-    converter = Babelish::CSV2Android.new(csv_file,
-                                          { "French" => "fr" },
+    converter = Babelish::CSV2Android.new(csv_file, { "French" => "fr" },
     :default_lang => "English",
     :comments_column => 5)
     converter.convert
