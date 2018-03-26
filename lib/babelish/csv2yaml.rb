f17032f9baee966318c0d07125b29b1ef35c82be
Nikolay Moskvin <nikolay.moskvin@sibext.com>
2018-03-26 17:05:14 +0700

    [csv2yaml] Added prefix and changed filename for yaml files

--- lib/babelish/csv2yaml.rb
+++ lib/babelish/csv2yaml.rb
@@ -2,33 +2,41 @@ module Babelish
   # Converter from csv format to Rails like yaml format
   class CSV2YAML < Csv2Base
     attr_accessor :is_first_row
+    attr_accessor :deep
 
     def initialize(filename, langs, args = {})
       super(filename, langs, args)
       @is_first_row = true
+      @deep = 1
     end
 
     def language_filepaths(language)
       require 'pathname'
       filename = @output_basename || language.code
-      filepath = Pathname.new("#{@output_dir}/#{filename}.#{extension}")
+      filepath = Pathname.new("#{@output_dir}/#{filename}.#{language.code}.#{extension}")
       @language = language
       filepath ? [filepath] : []
     end
 
-    def get_row_format(row_key, row_value, comment = nil, _indentation = 0)
+    def get_row_format(row_key, row_value, comment = nil, indentation = 0)
       entry = ''
+      indent = '  '
       if @is_first_row
-        entry += "#{@language.code}:\n"
+        entry << "#{@language.code}:\n"
+        @deep = 1
+        unless @output_basename.empty?
+          entry << "#{indent}#{@output_basename}:\n"
+          @deep = 2
+        end
         @is_first_row = false
       end
-      indent = '  '
-      entry << "#{indent}# #{comment}\n" unless comment.to_s.empty?
-      entry << "#{indent}#{row_key}: \"#{row_value}\"\n"
+      entry << "#{indent*@deep}# #{comment}\n" unless comment.to_s.empty?
+      entry << "#{indent*@deep}#{row_key}: \"#{row_value}\"\n"
     end
 
     def extension
       'yaml'
     end
+
   end
 end
